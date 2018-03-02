a6e688bcecc9c5f0f85a77c87cf5766c41cb0860
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-02 23:49:19 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -13,7 +13,7 @@ GROUP BY projects.title ORDER BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT users.name, pledges.amount FROM users
+"SELECT users.name, users.age pledges.amount FROM users
 INNER JOIN pledges ON pledges.user_id=users.id
 GROUP BY users.name ORDER BY users.name;"
 end
