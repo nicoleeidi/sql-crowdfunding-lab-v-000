bb733a8f9d2b5cff67286469ab26e27b2f4321ca
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-02 23:42:54 +0000

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -1,4 +1,4 @@
-CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal TEXT, start_date date, end_date date);
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INTEGER, start_date date, end_date date);
 CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
 CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER);
 
