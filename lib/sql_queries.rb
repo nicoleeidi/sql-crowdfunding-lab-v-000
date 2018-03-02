06c45b2ab851e4b6e290347e4ae5673039e6033d
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-02 23:57:06 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,7 +38,7 @@ WHERE projects.category= 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"SELECT projects.category, COUNT(*) FROM projects
+"SELECT projects.category, SUM(pledges.amount) FROM projects
 INNER JOIN pledges ON pledges.project_id=projects.id
 WHERE projects.category= 'books';"
 end
