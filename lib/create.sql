9a2c4726527e9f2c81be29d6c3784f11dfc53ede
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-02 22:44:29 +0000

    Done.

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,5 @@
-CREATE TABLE projects id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal TEXT, start_date date, end_date date);
-CREATE TABLE users id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
-CREATE TABLE pledges id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER);
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal TEXT, start_date date, end_date date);
+CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);
+CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount INTEGER, user_id INTEGER, project_id INTEGER);
 
 CREATE TABLE groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER, aisle INTEGER);
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,25 +9,35 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT projects.title, pledges.amount FROM projects
 INNER JOIN pledges ON pledges.project_id=projects.id
-ORDER BY projects.title"
+GROUP BY projects.title ORDER BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT users.name, pledges.amount "
+"SELECT users.name, pledges.amount FROM users
+INNER JOIN pledges ON pledges.user_id=users.id
+ORDER BY users.name;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT projects.title, pledges.amount-projects.funding_goal FROM projects
+INNER JOIN pledges ON pledges.project_id=projects.id
+WHERE pledges.amount >= projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"SELECT users.name, pledges.amount FROM users
+INNER JOIN pledges ON pledges.user_id=users.id
+GROUP BY users.name ORDER BY pledges.amount, users.name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT projects.category, pledges.amount FROM projects
+INNER JOIN pledges ON pledges.project_id=projects.id
+WHERE projects.category= 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+"SELECT projects.category, COUNT(*) FROM projects
+INNER JOIN pledges ON pledges.project_id=projects.id
+WHERE projects.category= 'books';"
 end
